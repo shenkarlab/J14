c031b55e75d0267d2d7f45370395948b29b20973
ohad sasson <oahdsasson@ohads-MacBook-Pro-2.local>
2015-12-04 18:43:37 +0200

    adding app structure

---
+++ app/index.js
@@ -0,0 +1,6 @@
+/* create express instance and listen to port 8080 */
+/* DONOT forget (cmd, at project folder): $npm install express -- save */
+var express = require('express');
+var app = express();
+app.use('/', express.static('./public')).listen(8080);
+console.log("We have connection ! Lift -off 8080");
