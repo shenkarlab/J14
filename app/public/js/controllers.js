585428fac9096e976c772a3b2f3468d6e035ac2e
maor toubian <maor toubian>
2016-01-30 00:31:58 +0200

    new story finished, new form with validations

--- app/public/js/controllers.js
+++ app/public/js/controllers.js
@@ -77,7 +77,7 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 					//default Rotchild blvd
 					$scope.coords ={latitude:32.0635743,longitude:34.7773985};
 					initialize($scope.mapStyle.data, $scope.coords);
-					mapObjectsCoor($scope.mapObjects);
+					setTimeout(function(){mapObjectsCoor($scope.mapObjects);},1000);
 				});
 			});
 		};
@@ -126,12 +126,12 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 
 		var createMarker = function (info , obj){
 
-								var path = '../../images/circle.svg';
+								var path = '../../images/tag-on-map.svg';
 								$scope.momObj = obj;
 								iterator++;
 
 								var markerImage = new google.maps.MarkerImage(
-								    '../../images/circle.svg',
+								    '../../images/tag-on-map.svg',
 								    new google.maps.Size(13,13), //size
 								    null, //origin
 								    null, //anchor
@@ -154,15 +154,17 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 
 								google.maps.event.addListener(marker, 'click', function(){	
 										$scope.rightNavContentStory(marker);
-										$("#right-nav-lead").css( "display", "none" );
+										//$("#right-nav-lead").css( "display", "none" );
 										$("#right-nav-story").css( "display", "block" );
+										$("#graph-nav").animate({bottom: '-500px'});
+										$("#right-nav-story").animate({right: '0px'});
 										noLeadClicked = false;
 										clearInterval(LeadsInterval);
 								});
 
 							//add info window when hover on maker show user name
 							var infoWindow = new google.maps.InfoWindow({
-								content: marker.content.userFname+" "+marker.content.userLname
+								content:'<p class="user-marker-window">'+marker.content.userFname+" "+marker.content.userLname+'</p>'
 							});
 							google.maps.event.addListener(marker,'mouseover',function(){
 									infoWindow.open($scope.map, marker);
@@ -179,17 +181,15 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 				$scope.$apply(function(){
 					if(angular.isDefined(marker)){
 
-						$scope.leadStory = "\" " + marker.content.whatIDid + " \"";
-						$scope.userInfo =  marker.content.userFname +" "+
-															 marker.content.userLname +" "+
-															 marker.content.age;
+						$scope.leadStory = "\" " + marker.content.userFname +" "+marker.content.userLname + " \"";
+						$scope.userInfo =   marker.content.age +" | " +marker.content.status16+" | "+marker.content.city16;
 
 						if(typeof tempMarker["id"] === 'undefined'){
-								marker.setIcon("../../images/circleSelected.svg");
+								marker.setIcon("../../images/tag-on-map.svg");
 						}
 						else if (parseInt(tempMarker["id"]) != marker["id"]){
-								tempMarker.setIcon("../../images/circle.svg");
-								marker.setIcon("../../images/circleSelected.svg");
+								tempMarker.setIcon("../../images/tag-on-map.svg");
+								marker.setIcon("../../images/tag-on-map.svg");
 						}
 						tempMarker = marker;
 					}
@@ -204,43 +204,64 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 
 			$scope.$apply(function(){
 
-					$scope.leadStory = "\" " + marker.content.whatIDid + " \"";
-					$scope.userInfo  = marker.content.userFname +" "+
-														 marker.content.userLname +" "+
-														 marker.content.age;
+					$scope.userName = marker.content.userFname +" "+marker.content.userLname;
+					var currentStatus;
+					if(marker.content.status16=="devorced"){currentStatus = "גרוש";}
+					else if(marker.content.status16=="single"){currentStatus = "רווק";}
+					else{currentStatus = "נשוי";}
 
+					$scope.userInfo =   marker.content.age +" | " +currentStatus+" | "+marker.content.city16;
 					$scope.protestStory =  marker.content.conclusion;
 
 					if(marker.content.rent11>marker.content.rent16){
-						$("#icon1").attr( "class", "icon rentDown" );
-					}else{
-						$("#icon1").attr( "class", "icon rentUp");
+						$("#icon1").attr( "class", "icon rentUp" );
+						$("#icon1Desc").attr( "class", "iconDesc green");
+						$("#icon1Desc").text("משלם פחות על שכר דירה מאז 2011");
+					}
+					else{
+						$("#icon1").attr( "class", "icon rentDown");
+						$("#icon1Desc").attr( "class", "iconDesc red");
+						$("#icon1Desc").text("משלם יותר על שכר דירה מאז 2011");
 					}
 
 					if(marker.content.salaryIncreased){
 						$("#icon2").attr( "class", "icon payUp");
-					}else{
+						$("#icon2Desc").attr( "class", "iconDesc green");
+						$("#icon2Desc").text("מרוויח יותר מאז 2011");
+					}
+					else{
 						$("#icon2").attr( "class", "icon payDown");
+						$("#icon2Desc").attr( "class", "iconDesc red");
+						$("#icon2Desc").text("מרוויח פחות מאז 2011");
 					}
 
 					if(marker.content.renewProtest){
-						$("#icon3").attr( "class", "icon renew");
-					}else{
-						$("#icon3").attr( "class", "icon notRenew");
+						$("#icon4").attr( "class", "icon renew");
+						$("#icon4Desc").attr( "class", "iconDesc yellow");
+						$("#icon4Desc").text("חושב שצריך לחדש את המחאה");
+					}
+					else{
+						$("#icon4").attr( "class", "icon notRenew");
+						$("#icon4Desc").text("לא חושב שצריך לחדש את המחאה");
 					}
 
 					if(marker.content.happy){
-						$("#icon4").attr( "class", "icon optimi");
-					}else{
-						$("#icon4").attr( "class", "icon pasimi");
+						$("#icon3").attr( "class", "icon optimi");
+						$("#icon3Desc").attr( "class", "iconDesc green");
+						$("#icon3Desc").text("אופטימי");
+					}
+					else{
+						$("#icon3").attr( "class", "icon pasimi");
+						$("#icon3Desc").attr( "class", "iconDesc red");
+						$("#icon3Desc").text("פאסימי");
 					}
 
 					if(typeof tempMarker["id"] === 'undefined'){
-							marker.setIcon("../../images/circleSelected.svg");
+							marker.setIcon("../../images/tag-on-map.svg");
 					}
 					else if (parseInt(tempMarker["id"]) != marker["id"]){
-							tempMarker.setIcon("../../images/circle.svg");
-							marker.setIcon("../../images/circleSelected.svg");
+							tempMarker.setIcon("../../images/tag-on-map.svg");
+							marker.setIcon("../../images/tag-on-map.svg");
 					}
 					tempMarker = marker;
 			});
@@ -264,7 +285,6 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 				console.log(event.latLng.lat()+" "+event.latLng.lng());
 				userLatitude = event.latLng.lat();
 				userLongitude = event.latLng.lng();
-				debugger;
 				placeMarker(event.latLng,map);
 			});
     }
@@ -285,6 +305,8 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
             $scope.rightNavContentStory($scope.markers[randomLead]);
 				$("#right-nav-lead").css( "display", "none" );
 				$("#right-nav-story").css( "display", "block" );
+				$("#right-nav-story").animate({right: '0px'});
+				$("#graph-nav").animate({bottom: '-500px'});
 				noLeadClicked = false;
 				clearInterval(LeadsInterval);
 		});
@@ -296,57 +318,6 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 				$scope.rightNavContentStory($scope.markers[randomLead]);
 		});
 
-		//hover listeners to the icons descriptions
-		$("#icon1").mouseout(function() {
-				$("#icon1Desc").text( "" );
-		}).mouseover(function() {
-				if($("#icon1").hasClass("rentUp")){
-					  $("#icon1Desc").attr( "class", "iconDesc green");
-						$("#icon1Desc").text("שכר הדירה עלה");
-				}else{
-					  $("#icon1Desc").attr( "class", "iconDesc red");
-						$("#icon1Desc").text("שכר הדירה ירד");
-				}
-		});
-
-		//hover listeners to the icons descriptions
-		$("#icon2").mouseout(function() {
-				$("#icon2Desc").text( "" );
-		}).mouseover(function() {
-				if($("#icon2").hasClass("payUp")){
-					  $("#icon2Desc").attr( "class", "iconDesc green");
-						$("#icon2Desc").text("שכר עלה");
-				}else{
-					  $("#icon2Desc").attr( "class", "iconDesc red");
-						$("#icon2Desc").text("שכר ירד");
-				}
-		});
-
-		//hover listeners to the icons descriptions
-		$("#icon3").mouseout(function() {
-				$("#icon3Desc").text( "" );
-		}).mouseover(function() {
-				if($("#icon3").hasClass("renew")){
-					  $("#icon3Desc").attr( "class", "iconDesc yellow");
-						$("#icon3Desc").text("בעד חידוש המחאה");
-				}else{
-						$("#icon3Desc").text("נגד חידוש המחאה");
-				}
-		});
-
-		//hover listeners to the icons descriptions
-		$("#icon4").mouseout(function() {
-				$("#icon4Desc").text( "" );
-		}).mouseover(function() {
-				if($("#icon4").hasClass("optimi")){
-					  $("#icon4Desc").attr( "class", "iconDesc green");
-						$("#icon4Desc").text("אופטימי");
-				}else{
-						$("#icon4Desc").text("פסימי");
-						$("#icon4Desc").attr( "class", "iconDesc red");
-				}
-		});
-
 		//loading from json and adding all the camps the the select option on the top
 		var campsList;
 		$http.get('../json/camps.json').then(function(json){
@@ -354,6 +325,17 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 						campsList +="<option value=\""+json.data.camps[i].camp+"\">" ;
 				}
 				$("#camps").append(campsList);
+		});
+
+
+		//loading from json and adding all the camps the the select option on the top
+		var citiesList;
+		$http.get('../json/cities.json').then(function(json){
+				for(var i=0 ;i<json.data.cities.length;i++){
+						citiesList +="<option value=\""+json.data.cities[i].name+"\">" ;
+				}
+				$("#cities11").append(citiesList);
+				$("#cities16").append(citiesList);
 		});
 
 		//draw chart example from our CDN
@@ -378,9 +360,10 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 	    function closeForm(){
 				$("#right-nav-form").animate({right: '-1000px'});
 				$("#right-nav-lead").animate({top: '0px'});
-				$("#right-nav-story").animate({top: '0px'});
 				$("#graph-nav").animate({bottom: '-22%'});
-  }
+  		}
+
+
 
 		//on click on the map a marker for share or add new will added to the map
 		//--todo-- the form and the buttoms beside the maker--//
@@ -388,16 +371,15 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 			if(!newMarkerAdded){
 
 				$("#right-nav-form").css( "display", "block" );
-
 				//animated version
 				$("#right-nav-form").animate({right: '0px'});
 				$("#right-nav-lead").animate({top: '-1500px'});
-				$("#right-nav-story").animate({top: '-1500px'});
+				$("#right-nav-story").animate({right: '-1000px'});
 				$("#graph-nav").animate({bottom: '-500px'});
 
 				iterator++;
 				var markerImage = new google.maps.MarkerImage(
-						'../../images/pendingCircle.svg',
+						'../../images/tag-on-map.svg',
 						new google.maps.Size(13,13), //size
 						null, //origin
 						null, //anchor
@@ -411,18 +393,63 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 						map:map
 				});
 
-				//add info window when hover on maker show pending status
-				var infoWindow = new google.maps.InfoWindow({
-					content: "תוכן ממתין לאישור"
+
+				var tags1 = '<div class="panel panel-default" ng-controller="MomentsListCtrl" ><div class="panel-heading panelHeadTakeMe panel-font" style =" ';
+				var tags11 = '">';
+				var tags2 = '</div><div class="panel-body panelBodyTakeMe" style =" ';
+				var tags22 = '"><p>';
+				var tags3 = '</p><a class=" panelBtntakeMe" role="button" href="#/moments/';
+				var tags4 ='"style=" ';
+				var tags444 = '">Take me</a></div><div class= "class="panel-footer panelFooterTakeMe panel-font " style=" ';
+				var tags44 = '<div class= "bodyPanelCnt " style =" ';
+				var tags5= '</div></div>';
+				
+				infoBubble = new InfoBubble({
+					shadowStyle: 0,
+					padding: 0,
+					backgroundColor: 'transparent',
+					borderRadius: 5,
+					arrowSize: 0,
+					borderWidth: 1,
+					borderColor: 'transparent',
+					disableAutoPan: true,
+					backgroundClassName: 'bubbleBody'
 				});
 
+				//add info window when hover on maker show pending status
+				var infoWindow = new google.maps.InfoWindow({
+					//content:'<p class="user-marker-window">fuckkkkkkkkkkkk</p>'
+					//content: "תוכן ממתין לאישור"
+				});
+
+				google.maps.event.addListener(newMarker,'mouseover',function(){
+					//infoWindow.open($scope.map, newMarker);
+					infoBubble.setContent( tags1   + tags11 + tags2 + tags22 + tags3 +  tags4 + tags444 + tags11 + tags5); 
+					infoBubble.open($scope.map, newMarker);
+
+				});
+
+				google.maps.event.addListener(newMarker,'mouseout',function(){
+					infoWindow.close($scope.map, newMarker);
+				});
+
+
+google.maps.event.addListener(infoWindow, 'domready', function() {
+    var l = $('.user-marker-window').parent().parent().parent().siblings();
+    for (var i = 0; i < l.length; i++) {
+        if($(l[i]).css('z-index') == 'auto') {
+            $(l[i]).css('border-radius', '16px 16px 16px 16px');
+            $(l[i]).css('border', '2px solid red');
+        }
+    }
+});
 				//map.setCenter(new google.maps.LatLng(marker.getPosition().lat(), marker.getPosition().lng()+1));
 				newMarkerAdded = true;
 			}
 		}
 
-	    //form next bottom function --TODO-- add validation to the fields
-		$("#form-next-botton").click(function(){
+		function nextPage(){
+
 			formPage++;
 			$("#form-prev-botton").css( "display", "block" );
 			if(formPage==1){
@@ -438,10 +465,132 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 				$("#form-fourth-page").css( "display", "block" );
 			}
 			else if(formPage==4){
-				$("#form-next-botton").css( "display", "none" );
-				$("#form-send-botton").css( "display", "block" );
 				$("#form-fourth-page").css( "display", "none" );
 				$("#form-fifth-page").css( "display", "block" );
+			}
+			else if(formPage==5){
+				$("#form-next-botton").css( "display", "none" );
+				$("#form-send-botton").css( "display", "block" );
+				$("#form-fifth-page").css( "display", "none" );
+				$("#form-sixed-page").css( "display", "block" );
+			}
+			$("#form-pagin-"+(formPage)).attr( "class", "form-pagin-close");
+			$("#form-pagin-"+(formPage+1)).attr( "class", "form-pagin-open");
+		}
+
+
+		function validatePagefleids(){
+			console.log("formPage: "+formPage);
+			if(formPage==0){
+
+				var name = $("#userName").val();
+				var lastName = $("#userLastName").val();
+				var age = $("#userAge").val();
+				if(name=="" || lastName==""){
+					$(".err").html("שם או שם המשפחה אינו תקין");
+					return false;
+				}
+				if( age=="" || age<1 || age> 99 || !$.isNumeric(age)){
+					$(".err").html("הגיל שהוזן אינו תקין");
+					return false;
+				}
+				$(".err").html("");
+				return true;
+
+			}
+			else if(formPage==1){
+
+				var didRent11= $scope.user.didRent11;
+				var rent11= $scope.user.r11;
+				var status11 = $scope.user.st11;
+				var cities11 = $scope.user.c11;
+
+				if(didRent11=="" || status11=="" || cities11==""){
+
+					if(didRent11=="yes"){
+						if(typeof rent11 === 'undefined' || rent11 == '' || rent11<1 || rent11>20000 ){
+							$(".err").html("שכר הדיר שהוזן אינו תקין");
+							return false;
+						}
+					}
+					else{
+						$(".err").html("המידע שהוזן אינו תקין");
+						return false;
+					}
+				}
+				$(".err").html("");
+				return true;
+
+			}
+			else if(formPage==2){
+
+				var didRent16= $scope.user.didRent16;
+				var rent16= $scope.user.r16;
+				var status16 = $scope.user.st16;
+				var cities16 = $scope.user.c16;
+
+				console.log("didRent16: "+didRent16);
+				console.log("status16: "+status16);
+				console.log("cities16: "+cities16);
+				console.log("rent16: "+rent16);
+
+				if(didRent16=="" ||status16==""|| cities16==""){
+
+					if(didRent16=="yes"){
+						if(typeof rent16 === 'undefined' || rent16 == '' || rent16<1 || rent16>20000 ){
+							$(".err").html("שכר הדיר שהוזן אינו תקין");
+							return false;
+						}
+					}
+					else{
+						$(".err").html("המידע שהוזן אינו תקין");
+						return false;
+					}
+				}
+				else{
+					$(".err").html("");
+					return true;
+				}
+
+			}
+			else if(formPage==3){
+
+				var q1= $scope.user.pressure;
+				var q2= $scope.user.gov;
+				var q3= $scope.user.success;
+				var q4= $scope.user.happy;
+				var q5= $scope.user.renew;
+
+				if(q1=="" ||q2==""|| q3==""|| q4==""|| q5==""){
+					$(".err").html("המידע שהוזן אינו תקין");
+					return false;
+				}
+				$(".err").html("");
+				return true;
+
+			}
+			else if(formPage==4){
+				var conc= $scope.user.conc;
+
+				if(conc==""){
+					$(".err").html("המידע שהוזן אינו תקין");
+					return false;
+				}
+				$(".err").html("");
+				return true;
+			}
+			else if(formPage==5){
+
+				$(".err").html("");
+				return true;
+			}
+		}
+
+
+	    //form next bottom function --TODO-- add validation to the fields
+		$("#form-next-botton").click(function(){
+			if(validatePagefleids()){
+				nextPage();
 			}
 		});
 
@@ -462,31 +611,42 @@ usersControllers.controller('MapCtrl', ['$scope','$routeParams', '$http','geoloc
 				$("#form-fourth-page").css( "display", "none" );
 			}
 			else if(formPage==3){
-				$("#form-next-botton").css( "display", "block" );
-				$("#form-send-botton").css( "display", "none" );
 				$("#form-fourth-page").css( "display", "block" );
 				$("#form-fifth-page").css( "display", "none" );
 			}
+			else if(formPage==4){
+				$("#form-next-botton").css( "display", "block" );
+				$("#form-send-botton").css( "display", "none" );
+				$("#form-fifth-page").css( "display", "block" );
+				$("#form-sixed-page").css( "display", "none" );
+			}
+			$("#form-pagin-"+(formPage+1)).attr( "class", "form-pagin-open");
+			$("#form-pagin-"+(formPage+2)).attr( "class", "form-pagin-close");
 		});
 
 		//form close votton function, it the user clicked on it the marker temp marker will be removed
 		$("#close-form").click(function(){
-
-				//animated version
 				$("#right-nav-form").animate({right: '-1000px'});
 				$("#right-nav-lead").animate({top: '0px'});
-				$("#right-nav-story").animate({top: '0px'});
+				//$("#right-nav-story").animate({top: '0px'});
 				$("#graph-nav").animate({bottom: '-22%'});
-
-				//const version
-				// $("#right-nav-form").css( "display", "none" );
-				// $("#right-nav-lead").css( "display", "block" );
-				// $("#right-nav-story").css( "display", "block" );
-				// $("#graph-nav").css( "display", "block" );
-
 				newMarker.setMap(null);
 				newMarkerAdded = false;
 		})
 
-  }
-]);
+		//form close votton function, it the user clicked on it the marker temp marker will be removed
+		$("#close-story").click(function(){
+				$("#right-nav-story").animate({right: '-1000px'});
+				$("#graph-nav").animate({bottom: '-22%'});
+		})
+
+		$scope.openRent = function(year,answer){
+			if(answer=="yes"){
+				$(".rent"+year).css( "display", "block" );
+			}
+			else{
+				$(".rent"+year).css( "display", "none" );
+			}
+	    }
+
+}]);
